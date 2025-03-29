b893b7c0fcbf37e6842156457ea2b03004e8a579
Abdullah Shoab <imshoab883@gmail.com>
2025-03-29 16:50:14 +0600

    Add files via upload

---
+++ Map/firstRepeatingElement.cpp
@@ -0,0 +1,33 @@
+/*Find The First Repeating Element of an Array*/
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+int firstRepeating(vector<int>&arr, int n){
+	map<int,int>m;
+
+	for(auto it:arr){
+		m[it]++;
+		if(m[it]>1){
+			return it;
+		}
+	}
+	return -1;
+}
+
+int main(){
+	int n;
+	cin>>n;
+
+	vector<int>arr(n);
+
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+
+	int ans = firstNonRepeating(arr,n);
+	cout<<ans<<endl;
+
+	return 0;
+}
